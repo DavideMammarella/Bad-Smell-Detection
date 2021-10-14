305316558e404a2d240eaa7ed6324b477b539b78
Davide Mammarella <davide.mammarella@usi.ch>
2021-10-14 15:12:05 +0200

    Add populateOntology

--- individ-creator.py
+++ individ-creator.py
@@ -1,27 +1,47 @@
 from owlready2 import *
 import ast
-import javalang
-
+import javalang  # https://github.com/c2nes/javalang
 
 
 def populateOntology(onto, tree):
-    #class that populate the ontology
-    print(tree)
-    print("Entered the class!")
+    for node in tree.types:
+        if type(node) is javalang.tree.ClassDeclaration:  # for each ClassDeclaration in the javalang parse tree
+            cd = onto["ClassDeclaration"]()  # create an instance of the ontology class ClassDeclaration
+            cd.jname = [node.name]
+
+            for body_cd in node.body:
+                if type(body_cd) is javalang.tree.MethodDeclaration:
+                    md = onto["MethodDeclaration"]()
+                    md.jname = [node.name]
+                    cd.body.append(md)
+                elif type(body_cd) is javalang.tree.FieldDeclaration:
+                    fd = onto["FieldDeclaration"]()
+                    fd.jname = [node.name]
+                    cd.body.append(fd)
+                elif type(body_cd) is javalang.tree.ConstructorDeclaration:
+                    cdec = onto["ConstructorDeclaration"]()
+                    cdec.jname = [node.name]
+                    cd.body.append(cdec)
+            """
+                For each class member (MethodDeclaration/FieldDeclaration/ConstructorDeclaration) in the
+                "body" of a ClassDeclaration create a MethodDeclaration/FieldDeclaration/ConstructorDeclaration.
+                    
+                Instance and add (append) the member instance to the property "body" of the ClassDeclaration instance
+            """
 
 
 def main():
     onto = get_ontology("output/tree_section1.owl").load()  # load the ontology created in step 1 (onto-creator.py)
-    # loop because you need every javafile
+    # TODO: Ask if it is possible to upload multiple file in plain Python (not Panda)
     javafile = open(
-            "input/android-chess/app/src/main/java/jwtc/chess/Pos.java")  # get a java file of android-chess as input file
-    tree_of_javafile = javalang.parse.parse(javafile.read())  # get the TREE of input file
-    populateOntology(onto, tree_of_javafile)
+        "input/android-chess/app/src/main/java/jwtc/chess/Pos.java").read()  # get a java file of android-chess as input file
+    tree_of_javafile = javalang.parse.parse(javafile)  # get the TREE of input file
+    populateOntology(onto, tree_of_javafile) # populate the ontology
     onto.save(file="output/tree_section2.owl", format="rdfxml")  # save the ontology
 
 
 # def test_ontology():
-#     onto = get_ontology("tree_section2.owl").load()
+#     onto = get_ontology("output/tree_section2.owl").load()
 #     tree = javalang.parse.parse("class A { int x, y; }")
 #     populateOntology(onto, tree)
 #     a = onto['ClassDeclaration'].instances()[0]
@@ -30,8 +50,9 @@ def main():
 #     assert a.body[1].is_a[0].name == 'FieldDeclaration'
 #     assert a.body[1].jname[0] == 'y'
 #     print("Ontology is fine!")
+# TODO: Ask why the method seems working on a java class but not on the unit test
 
 
 if __name__ == "__main__":
     main()
-    #test_ontology()
+    # test_ontology()
