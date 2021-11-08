3b33998f1cef171cd88b6b44ad63b449cbb27716
Davide Mammarella <davide.mammarella@usi.ch>
2021-11-08 13:19:14 +0100

    Fix output (no empty tables from empty query results)

--- bad-smells.py
+++ bad-smells.py
@@ -4,7 +4,9 @@ import rdflib.plugins.sparql as sq
 from owlready2 import *
 import importlib
 
-individ_creator = importlib.import_module("individ-creator")
+# Global variables used only for Latex output purpose
+tab_number = 0
+bad_smells = []
 
 def writeLogBadSmells(bad_smells):
     with open("log.txt", "a+") as file_object:
@@ -13,23 +15,27 @@ def writeLogBadSmells(bad_smells):
             file_object.write(row)
         file_object.write("\\hline\n\\end{tabular}\n\\caption{Bad smells (Total).}\n\\label{table:badSmellTable}\n\\end{table}\n")
 
-def writeLog(title, query_result, tab_number, bad_smells):
+def writeLog(title, query_result, tab_number):
     with open("log.txt", "a+") as file_object:
-        file_object.write("\n\\begin{table}[H]\n\\centering \\scriptsize\n\\begin{tabular}{c c c}\n\\hline\nClass Name & Method Name & Number Of Instances\\\\\n\\hline\n")
-        count = 0
-        for row in query_result:
-            file_object.write(row.cn)
-            file_object.write(" & ")
-            file_object.write(row.mn)
-            file_object.write(" & ")
-            file_object.write(str(int(row.tot)))
-            file_object.write(" \\\\\n")
-            count = count + 1
-        file_object.write("\\hline\n\\end{tabular}\n\\caption{"+title+".}\n\\label{table:tab"+str(tab_number)+"}\n\\end{table}\n\n\n")
-        bad_smells.append(title+" & "+str(count)+" \\\\\n")
+        if len(query_result):
+            file_object.write("\n\\begin{table}[H]\n\\centering \\scriptsize\n\\begin{tabular}{c c c}\n\\hline\nClass Name & Method Name & Number Of Instances\\\\\n\\hline\n")
+            tab_number = tab_number + 1
+            count = 0
+            for row in query_result:
+                file_object.write(row.cn)
+                file_object.write(" & ")
+                file_object.write(row.mn)
+                file_object.write(" & ")
+                file_object.write(str(int(row.tot)))
+                file_object.write(" \\\\\n")
+                count = count + 1
+            file_object.write("\\hline\n\\end{tabular}\n\\caption{"+title+".}\n\\label{table:tab"+str(tab_number)+"}\n\\end{table}\n\n\n")
+            bad_smells.append(title+" & "+str(count)+" \\\\\n")
+        else:
+            bad_smells.append(title + " & 0 \\\\\n")
 
 
-def testQuery(g, bad_smells):
+def testQuery(g):
     q = sq.prepareQuery(
         """SELECT ?mn ?cn (COUNT(*) AS ?tot) WHERE {
                 ?c a tree:ClassDeclaration .
@@ -42,10 +48,10 @@ def testQuery(g, bad_smells):
 
     query_result = g.query(q)
     title = "Test:"
-    writeLog(title, query_result, 0, bad_smells)
+    writeLog(title, query_result, 0)
 
 # Long Methods: >= 20 statements
-def findLongMethods(g, bad_smells):
+def findLongMethods(g):
     q = sq.prepareQuery(
         """SELECT ?mn ?cn (COUNT(*) AS ?tot) WHERE {
                 ?class a tree:ClassDeclaration .
@@ -61,10 +67,10 @@ def findLongMethods(g, bad_smells):
 
     query_result = g.query(q)
     title = "Long Methods"
-    writeLog(title, query_result, 1, bad_smells)
+    writeLog(title, query_result, 1)
 
 # Long Constructor: >= 20 statements
-def findLongConstructors(g, bad_smells):
+def findLongConstructors(g):
     q = sq.prepareQuery(
         """SELECT ?cname ?cn (COUNT(*) AS ?tot) WHERE {
                 ?class a tree:ClassDeclaration .
@@ -80,10 +86,10 @@ def findLongConstructors(g, bad_smells):
 
     query_result = g.query(q)
     title = "Long Constructors"
-    writeLog(title, query_result, 2, bad_smells)
+    writeLog(title, query_result, 2)
 
 # LargeClass: >= 10 methods
-def findLargeClasses(g, bad_smells):
+def findLargeClasses(g):
     q = sq.prepareQuery(
         """SELECT ?mn ?cn (COUNT(*) AS ?tot) WHERE {
                 ?class a tree:ClassDeclaration .
@@ -97,10 +103,10 @@ def findLargeClasses(g, bad_smells):
 
     query_result = g.query(q)
     title = "Large Classes"
-    writeLog(title, query_result, 3, bad_smells)
+    writeLog(title, query_result, 3)
 
 # MethodWithSwitch: >= 1 switch statement in method/constructor body
-def findMethodsWithSwitch(g, bad_smells):
+def findMethodsWithSwitch(g):
     q = sq.prepareQuery(
         """SELECT ?mn ?cn (COUNT(*)AS ?tot) WHERE {
                 ?class a tree:ClassDeclaration .
@@ -117,10 +123,10 @@ def findMethodsWithSwitch(g, bad_smells):
 
     query_result = g.query(q)
     title = "Methods with Switch"
-    writeLog(title, query_result, 4, bad_smells)
+    writeLog(title, query_result, 4)
 
 # ConstructorWithSwitch: >= 1 switch statement in method/constructor body
-def findConstructorsWithSwitch(g, bad_smells):
+def findConstructorsWithSwitch(g):
     q = sq.prepareQuery(
         """SELECT ?mn ?cn (COUNT(*)AS ?tot) WHERE {
                 ?class a tree:ClassDeclaration .
@@ -137,10 +143,10 @@ def findConstructorsWithSwitch(g, bad_smells):
 
     query_result = g.query(q)
     title = "Constructors with Switch"
-    writeLog(title, query_result, 5, bad_smells)
+    writeLog(title, query_result, 5)
 
 # MethodWithLongParameterList: >= 5 parameters
-def findMethodsWithLongParameterList(g, bad_smells):
+def findMethodsWithLongParameterList(g):
     q = sq.prepareQuery(
         """SELECT ?mn ?cn (COUNT(*)AS ?tot) WHERE {
                 ?class a tree:ClassDeclaration .
@@ -156,10 +162,10 @@ def findMethodsWithLongParameterList(g, bad_smells):
 
     query_result = g.query(q)
     title = "Methods with Long Parameter List"
-    writeLog(title, query_result, 6, bad_smells)
+    writeLog(title, query_result, 6)
 
 # ConstructorWithLongParameterList: >= 5 parameters
-def findConstructorsWithLongParameterList(g, bad_smells):
+def findConstructorsWithLongParameterList(g):
     q = sq.prepareQuery(
         """SELECT ?mn ?cn (COUNT(*)AS ?tot) WHERE {
                 ?class a tree:ClassDeclaration .
@@ -175,10 +181,10 @@ def findConstructorsWithLongParameterList(g, bad_smells):
 
     query_result = g.query(q)
     title = "Constructors with Long Parameter List"
-    writeLog(title, query_result, 7, bad_smells)
+    writeLog(title, query_result, 7)
 
 # DataClass: class with only setters and getters
-def findDataClasses(g, bad_smells):
+def findDataClasses(g):
     q = sq.prepareQuery(
         """SELECT ?mn ?cn (COUNT(*)AS ?tot) WHERE {
                 ?class a tree:ClassDeclaration .
@@ -196,24 +202,22 @@ def findDataClasses(g, bad_smells):
 
     query_result = g.query(q)
     title = "Classes with only Setters and Getters"
-    writeLog(title, query_result, 8, bad_smells)
+    writeLog(title, query_result, 8)
 
 
 def main():
     g = rdflib.Graph()
     g.load("tree2.owl")
     open("log.txt", "w").close()  # erase the log on every start
-    bad_smells = []
     # queries
-    # testQuery(g)
-    findLongMethods(g, bad_smells)
-    findLongConstructors(g, bad_smells)
-    findLargeClasses(g, bad_smells)
-    findMethodsWithSwitch(g, bad_smells)
-    findConstructorsWithSwitch(g, bad_smells)
-    findMethodsWithLongParameterList(g, bad_smells)
-    findConstructorsWithLongParameterList(g, bad_smells)
-    findDataClasses(g, bad_smells)
+    findLongMethods(g)
+    findLongConstructors(g)
+    findLargeClasses(g)
+    findMethodsWithSwitch(g)
+    findConstructorsWithSwitch(g)
+    findMethodsWithLongParameterList(g)
+    findConstructorsWithLongParameterList(g)
+    findDataClasses(g)
     # write smells detection table
     writeLogBadSmells(bad_smells)
 
