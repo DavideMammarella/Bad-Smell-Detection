0765aa1d850e8df69c17c25b7f6e46a5347906c5
Davide Mammarella <davide.mammarella@usi.ch>
2021-11-15 13:03:21 +0100

    Code refactor

--- individ-creator.py
+++ individ-creator.py
@@ -5,57 +5,58 @@ from os.path import isfile
 import javalang  # https://github.com/c2nes/javalang
 
 
+def createDeclaration(body_cd, class_declaration, body_declaration):
+    """
+    For each instance in the "body" of a ClassDeclaration add (append) the member instance
+    to the property "body" of the ClassDeclaration instance.
+    """
+    body_declaration.jname = [body_cd.name]
+    class_declaration.body.append(body_declaration)
+
+
+def createStatementsAndParameters(onto, body_cd, declaration):
+    """
+    For each Statement (IfStatement, WhileStatement, etc.) reachable from the body of a
+    MethodDeclaration or ConstructorDeclaration, create a Statement instance  and add (append)
+    the Statement instances to the property "body" of the MethodDeclaration or ConstructorDeclaration
+
+    For each parameter in the parameter list of a MethodDeclaration or ConstructorDeclaration,
+    create an instance of class FormalParameter and add (append) the FormalParameter instances
+    to the property "parameters" of the MethodDeclaration or ConstructorDeclaration
+    """
+    for _, statement in body_cd:
+        statement_name = type(statement).__name__
+        statement_instance = onto[statement_name]()
+        declaration.body.append(statement_instance)
+    for parameter in body_cd.parameters:
+        parameter_name = type(parameter).__name__
+        parameter_instance = onto[parameter_name]()
+        declaration.parameters.append(parameter_instance)
+
+
 def populateOntology(onto, tree):
-    for _, node in tree.filter(javalang.tree.ClassDeclaration):  # for each ClassDeclaration in the javalang parse tree
-            cd = onto["ClassDeclaration"]()  # create an instance of the ontology class ClassDeclaration
-            cd.jname = [node.name]
-            for body_cd in node.body:
-                if type(body_cd) is javalang.tree.MethodDeclaration:
-                    md = onto["MethodDeclaration"]()
-                    md.jname = [body_cd.name]
-                    cd.body.append(md)
-                    # create statement instances (first element is the method declaration!)
-                    for _, statement in body_cd:
-                        statement_name = type(statement).__name__
-                        statement_instance = onto[statement_name]()
-                        md.body.append(statement_instance)
-                    # create parameter instances
-                    for parameter in body_cd.parameters:
-                        parameter_name = type(parameter).__name__
-                        parameter_instance = onto[parameter_name]()
-                        md.parameters.append(parameter_instance)
-                elif type(body_cd) is javalang.tree.ConstructorDeclaration:
-                    cdec = onto["ConstructorDeclaration"]()
-                    cdec.jname = [body_cd.name]
-                    cd.body.append(cdec)
-                    # create statement instances (first element is the constructor declaration!)
-                    for _, statement in body_cd:
-                        statement_name = type(statement).__name__
-                        statement_instance = onto[statement_name]()
-                        cdec.body.append(statement_instance)
-                    # create parameter instances
-                    for parameter in body_cd.parameters:
-                        parameter_name = type(parameter).__name__
-                        parameter_instance = onto[parameter_name]()
-                        cdec.parameters.append(parameter_instance)
-                elif type(body_cd) is javalang.tree.FieldDeclaration:
-                    for field in body_cd.declarators:
-                        fd = onto["FieldDeclaration"]()
-                        fd.jname = [field.name]
-                        cd.body.append(fd)
-            """
-                For each class member (MethodDeclaration/FieldDeclaration/ConstructorDeclaration) in the
-                "body" of a ClassDeclaration create a MethodDeclaration/FieldDeclaration/ConstructorDeclaration
-                instance and add (append) the member instance to the property "body" of the ClassDeclaration instance
-                
-                For each Statement (IfStatement, WhileStatement, etc.) reachable from the body of a
-                MethodDeclaration or ConstructorDeclaration, create a Statement instance  and add (append) 
-                the Statement instances to the property "body" of the MethodDeclaration or ConstructorDeclaration
-                
-                For each parameter in the parameter list of a MethodDeclaration or ConstructorDeclaration, 
-                create an instance of class FormalParameter and add (append) the FormalParameter instances 
-                to the property "parameters" of the MethodDeclaration or ConstructorDeclaration
-            """
+    """
+    For each ClassDeclaration in the javalang parse tree create an instance of the ontology class ClassDeclaration.
+
+    For each class member (MethodDeclaration/FieldDeclaration/ConstructorDeclaration) in the
+    "body" of a ClassDeclaration create a MethodDeclaration/FieldDeclaration/ConstructorDeclaration instance.
+    """
+    for _, node in tree.filter(javalang.tree.ClassDeclaration):
+        cd = onto["ClassDeclaration"]()
+        cd.jname = [node.name]
+        for body_cd in node.body:
+            if type(body_cd) is javalang.tree.MethodDeclaration:
+                md = onto["MethodDeclaration"]()
+                createDeclaration(body_cd, cd, md)
+                createStatementsAndParameters(onto, body_cd, md)
+            elif type(body_cd) is javalang.tree.ConstructorDeclaration:
+                cdec = onto["ConstructorDeclaration"]()
+                createDeclaration(body_cd, cd, cdec)
+                createStatementsAndParameters(onto, body_cd, cdec)
+            elif type(body_cd) is javalang.tree.FieldDeclaration:
+                for field in body_cd.declarators:
+                    fd = onto["FieldDeclaration"]()
+                    createDeclaration(field, cd, fd)
 
 
 def main():
